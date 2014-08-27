0f87134deac067ab9e66fd0c9e32d83de9811178
Stephen Holtz <stephenlholtz@gmail.com>
2014-08-27 16:12:52 -0400

    baby step test blanks

--- monkeylogic/channelrhodopsin_gabor/opto_blank_2s_gab.m
+++ monkeylogic/channelrhodopsin_gabor/opto_blank_2s_gab.m
@@ -1,19 +1,27 @@
-% Blank screen (unrewarded) with 2 second stimulus duration
+% Blank screen (unrewarded) with 2 second stimulus duration 
+% AND surrounding 1 sec of opto stimulation
 %
-% Modified from RR and RF scripts
 % SLH
 
-disp('Stimulus: Blank')
+disp('Stimulus: Blank with Opto')
 
 % Task object for timing file
 taskObjBlank = 1;
+optoObj      = 2;
 
 % Define time intervals (in ms)
-stimulusDuration        = 2000;     % Time the video is playing
-slopTime                = 40;       % To prevent crashes, inserted gaps between commands...
+preStimOptoDuration  = 500;      % Time before stimulus to use opto
+postStimOptoDuration = 500;      % Time after stimulus to use opto
+stimulusDuration     = 2000;     % Time the video is playing
+slopTime             = 40;       % To prevent crashes, inserted gaps between commands...
 
-totalConditionDuration  = stimulusDuration + slopTime*2; % Total time for the entire condition
+ % Total time for the entire condition
+totalConditionDuration  = preStimOptoDuration + postStimOptoDuration + stimulusDuration + slopTime*2;
 fprintf('Ideal condition time: %d ms\n',totalConditionDuration);
+
+% Start the opto
+toggleobject(punishmentObj, 'Status','On');
+idle(preStimOptoDuration)
 
 % Threshold for counting a lick (ON/OFF is 6V/0V)
 lickThreshold = 4;
@@ -39,4 +47,7 @@ end
 % No reward regardless of lick
 idle(slopTime);
 toggleobject(taskObjBlank,'status','off','Eventmarker',24);
-idle(slopTime);
+
+% Turn off the opto stim
+idle(postStimOptoDuration)
+toggleobject(optoObj, 'Status','Off');
--- monkeylogic/channelrhodopsin_gabor/opto_blank_2s_gab_reward.m
+++ monkeylogic/channelrhodopsin_gabor/opto_blank_2s_gab_reward.m
@@ -3,18 +3,26 @@
 % Modified from RR and RF scripts
 % SLH
 
-disp('Stimulus: Blank with reward')
+disp('Stimulus: Blank Opto with reward')
 
 % Task object for timing file
 taskObjBlank = 1;
+optoObj      = 2;
 
 % Define time intervals (in ms)
-stimulusDuration = 2000;     % Time the video is playing
-slopTime         = 50;       % To prevent crashes, inserted gaps between commands...
-solenoidDuration = 150;      % Open time for solenoid valve, requires calibration
+preStimOptoDuration  = 500;      % Time before stimulus to use opto
+postStimOptoDuration = 500;      % Time after stimulus to use opto
+stimulusDuration     = 2000;     % Time the video is playing
+slopTime             = 50;       % To prevent crashes, inserted gaps between commands...
+solenoidDuration     = 225;      % Open time for solenoid valve, requires calibration
 
-totalConditionDuration  = stimulusDuration + slopTime*2; % Total time for the entire condition
+ % Total time for the entire condition
+totalConditionDuration  = preStimOptoDuration + postStimOptoDuration + stimulusDuration + solenoidDuration + slopTime*2;
 fprintf('Ideal condition time: %d ms\n',totalConditionDuration);
+
+% Start the opto
+toggleobject(punishmentObj, 'Status','On');
+idle(preStimOptoDuration)
 
 % Threshold for counting a lick (ON/OFF is 6V/0V)
 lickThreshold = 4;
@@ -38,4 +46,7 @@ else
 end
 idle(slopTime);
 toggleobject(taskObjBlank,'status','off','Eventmarker',25);
-idle(slopTime);
+
+% Turn off the opto stim
+idle(postStimOptoDuration)
+toggleobject(optoObj, 'Status','Off');
